78d078892119a6cb69a15b171e313c80627e12f5
Joel Muysekns <jrmuyskens@gmail.com>
2022-12-23 13:23:18 -0500

    feat: create game types package

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const CONFIG = require('@config');
+const CONFIG = require('config');
 
 const app = express();
 
 // TODO: Write API
-
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
